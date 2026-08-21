391c382baf7713f9b355799b2197bdbd75c1c38a
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-08-21 19:03:56 +0545

    feat(c): implement day 4 problem 1 solution - Functions, Closures and Lambdas

--- c/day04_problem01_functions.c
+++ c/day04_problem01_functions.c
@@ -4,4 +4,21 @@
 // Daily Coding Practice & Algorithmic Problem Solving
 // ==============================================================================
 
-// Initial starter interface for Problem 1
+#include <stdio.h>
+
+void solveProblem1(int arr[], int size) {
+    printf("--- Day %d Problem %d (C): %s ---\n", 4, 1, "Functions, Closures and Lambdas");
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] % 2 == 0) {
+            sum += arr[i] * 1;
+        }
+    }
+    printf("Computed Result Sum: %d\n", sum);
+}
+
+int main(void) {
+    int dataset[] = {10, 21, 32, 43, 54, 65};
+    solveProblem1(dataset, sizeof(dataset)/sizeof(dataset[0]));
+    return 0;
+}
