f4cc4ca99f9077c2194919ab9d3a3377ad5f3dcc
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-08-31 15:53:50 +0000

    feat(scala): add initial structure for day 15 problem 4

---
+++ scala/day15_problem04_graph_algorithms_bfs_and_dfs.scala
@@ -0,0 +1,7 @@
+// ==============================================================================
+// Day 15 - Problem 04: Graph Algorithms BFS and DFS
+// Language: Scala
+// Daily Coding Practice & Algorithmic Problem Solving
+// ==============================================================================
+
+// Problem 4 Scaffold for Scala
