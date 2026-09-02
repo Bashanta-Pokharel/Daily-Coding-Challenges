67d2d1a81df51cbcd2b61b9976bb2224239202ea
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-02 16:53:54 +0000

    feat(lua): add initial structure for day 19 problem 5

---
+++ lua/day19_problem05_file_io_and_data_serialization.lua
@@ -0,0 +1,7 @@
+-- ==============================================================================
+-- Day 19 - Problem 05: File IO and Data Serialization
+-- Language: Lua
+-- Daily Coding Practice & Algorithmic Problem Solving
+-- ==============================================================================
+
+-- Problem 5 Scaffold for Lua
