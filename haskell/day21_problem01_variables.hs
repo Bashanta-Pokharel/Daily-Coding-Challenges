6f94f1d54191513b482ad126b4ed787d5a630b8f
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-04 04:32:01 +0000

    feat(haskell): implement day 21 problem 1 solution - Variables, Types and Memory Models

--- haskell/day21_problem01_variables.hs
+++ haskell/day21_problem01_variables.hs
@@ -4,4 +4,5 @@
 -- Daily Coding Practice & Algorithmic Problem Solving
 -- ==============================================================================
 
--- Problem 1 Scaffold for Haskell
+-- Implementation for Day 21 Problem 1 in Haskell
+-- Topic: Variables, Types and Memory Models
