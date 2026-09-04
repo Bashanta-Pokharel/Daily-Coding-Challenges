859111a83976266c5b5364ac7d71f27f697ef75e
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-04 04:32:01 +0000

    feat(haskell): add initial structure for day 21 problem 4

---
+++ haskell/day21_problem04_variables.hs
@@ -0,0 +1,7 @@
+-- ==============================================================================
+-- Day 21 - Problem 04: Variables, Types and Memory Models
+-- Language: Haskell
+-- Daily Coding Practice & Algorithmic Problem Solving
+-- ==============================================================================
+
+-- Problem 4 Scaffold for Haskell
