a9a9caf54d8a052c41c6d3d2b027489c0c1812df
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-07 04:40:19 +0000

    test(javascript): add test cases and verification for day 24 problem 3

--- javascript/day24_problem03_functions.js
+++ javascript/day24_problem03_functions.js
@@ -9,3 +9,6 @@ function solveProblem3(inputArray) {
   const sum = filtered.reduce((acc, curr) => acc + curr, 0);
   return { day: 24, problem: 3, items: filtered, total: sum };
 }
+
+const sampleData = [12, 25, 34, 48, 55, 60];
+console.log("Solution Output:", solveProblem3(sampleData));
