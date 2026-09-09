d65ddb54681979389a2ca857edfbec7f7bd53677
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-09 04:41:05 +0000

    test(c): add test cases and verification for day 26 problem 5

--- c/day26_problem05_string_algorithms_and_parsing.c
+++ c/day26_problem05_string_algorithms_and_parsing.c
@@ -14,3 +14,9 @@ void solveProblem5(int arr[], int size) {
     }
     printf("Computed Result Sum: %d\n", sum);
 }
+
+int main(void) {
+    int dataset[] = {10, 21, 32, 43, 54, 65};
+    solveProblem5(dataset, sizeof(dataset)/sizeof(dataset[0]));
+    return 0;
+}
