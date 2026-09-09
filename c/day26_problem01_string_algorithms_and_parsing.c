6b8bbe68eaf40c71f054e71bf2f9331c7ce6dbbd
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-09 04:41:04 +0000

    feat(c): implement day 26 problem 1 solution - String Algorithms and Parsing

--- c/day26_problem01_string_algorithms_and_parsing.c
+++ c/day26_problem01_string_algorithms_and_parsing.c
@@ -6,4 +6,11 @@
 
 #include <stdio.h>
 
-void solveProblem1(int arr[], int size);
+void solveProblem1(int arr[], int size) {
+    printf("--- Day %d Problem %d (C): %s ---\n", 26, 1, "String Algorithms and Parsing");
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] % 2 == 0) sum += arr[i] * 1;
+    }
+    printf("Computed Result Sum: %d\n", sum);
+}
