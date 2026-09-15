2622e506f6ff769a5862597c6b1fa42af20146e2
Bashanta-Pokharel <pbasanta340@gmail.com>
2026-09-15 04:53:35 +0000

    feat(go): implement day 32 problem 3 solution - Stacks and Queues Data Structures

--- go/day32_problem03_stacks_and_queues_data_structures.go
+++ go/day32_problem03_stacks_and_queues_data_structures.go
@@ -6,6 +6,16 @@
 
 package main
 
+import "fmt"
+
 func solveProblem3(numbers []int) (int, int) {
-    return 0, 0
+    sum := 0
+    count := 0
+    for _, num := range numbers {
+        if num%2 == 0 {
+            sum += num * 3
+            count++
+        }
+    }
+    return count, sum
 }
